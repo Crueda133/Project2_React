ba257966adfc2da67c62a32252f9c3ed1baba26a
Erian Flores <erian.flores@gmail.com>
2024-12-03 11:35:05 -0600

    Updated

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,39 +19,49 @@ function Carousel() {
   }, []);
 
   return (
-    <div
-      id="carouselExampleAutoplaying"
-      className="carousel slide"
-      data-bs-ride="carousel"
-    >
-      <div className="carousel-inner">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`carousel-item ${index === 0 ? "active" : ""}`} // La primera imagen debe ser "active"
-          >
-            <img src={image.url} className="d-block w-100" alt={image.alt} />
+    <div>
+      <nav className="custom-navbar">
+        <div
+          id="carouselAutoplaying"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className={`carousel-item ${index === 0 ? "active" : ""}`} // La primera imagen debe ser "active"
+              >
+                <img src={image.url} className="d-block" alt={image.alt} />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleAutoplaying"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleAutoplaying"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselAutoplaying"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselAutoplaying"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </nav>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,25 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>
+    <div className="custom-navbar">
       {/* Navbar */}
       <nav
-        className="navbar navbar-expand-lg navbar-dark bg-dark"
-        style={{ background: "rgba(0, 0, 0, 0.6)" }}
+      // className="navbar navbar-expand-lg navbar-dark bg-dark"
+      // style={{ background: "rgba(0, 0, 0, 0.6)" }}
       >
         <div className="container">
           <Link className="navbar-brand" to="/">
-            HotelFinder
+            HomeHeaven
           </Link>
-          <button
+          {/* <button
             className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNav"
           >
             <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          </button> */}
+          <div id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
                 <Link className="nav-link" to="/favorites">
